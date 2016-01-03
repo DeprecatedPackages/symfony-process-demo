5c4f7922212ad692d84421c2a3e0b0676dfde8ca
Ondřej Machulda <ondrej.machulda@gmail.com>
2016-01-03 16:25:48 +0100

    Example of Process usage inside a Console Command

---
+++ cli.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+//cli.php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$console = new Application('Symfony Console demo for Zdroják.cz', '4.5.6');
+
+$console->addCommands(
+    [
+        new App\Command\WebshotSimpleCommand(),
+    ]
+);
+$console->run();
